28453a65111e2258143e78a0a69ee5d0959de797
Afonso -aptsharp <aff-on@hotmail.com>
2020-04-21 22:05:30 -0300

    Criação do projeto - Criação da classe QRCoderController - Intalação do pacote NuGet QRCoder (Raffael Herrmann)

---
+++ Controllers/QRCoderController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.IO;
+
+namespace QRCodeAplicativo.Controllers
+{
+    public class QRCoderController : Controller
+    {
+        public IActionResult Index()
+            //Interface de codigo chamando o index()
+        {
+            return View();
+            // retorna um view ou seja uma pagina view.
+        }
+
+        [HttpPost]
+        // token para fazer um posto no codigo
+        public IActionResult Index(string qrTexto)
+            // puxando de uma interface e passando como parametro um texto
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            // estanciando/criando um novo objeto QRCodeGenerator
+            QRCodeData qRCodeData = qrGenerator.CreateQrCode(qrTexto, QRCodeGenerator.ECCLevel.Q);
+            // manda para a biblioteca a string
+            QRCode qRCode = new QRCode(qRCodeData);
+            // estancia um novo objeto QRCode passando como pareametro o qRCodeData
+            Bitmap qrCodeImage = qRCode.GetGraphic(10);
+            // cria a imagem QR e usa a biblioteca Graphic para escolher o tamanho da imagem 
+            return View(BitmapToBytes(qrCodeImage));
+            // retorna a image QRCode na para a view que ira exibir na tela. 
+        }
+
+        private static Byte[] BitmapToBytes(Bitmap img)
+        // classe privada apenas para uso do BitmapToBytes // sem esta calsse o metodo BitmapToBytes da erro esperando algum retorno
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                // puxa a formulação da imagem depois que ela é gerada
+                return stream.ToArray();
+                // retorna a imagem
+            }
+        }
+    }
+}
